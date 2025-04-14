5ed803a499bb762f949eff0f53b51041095a5bc5
samarEissa <161602753+samarEissa@users.noreply.github.com>
2025-04-14 11:18:28 +0200

    finishing details screen

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/model/categorie.dart';
 import 'package:flutter_app/model/hostByIdModel.dart';
+import 'package:flutter_app/screens/favor.dart';
+import 'package:flutter_app/screens/footer.dart';
 import 'package:flutter_app/screens/nav.dart';
 import 'package:flutter_app/services/hostByIdServices.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class details extends StatefulWidget {
   const details({super.key, required this.id});
@@ -23,6 +27,19 @@ class _detailsState extends State<details> {
   late HostByIdModel oneHost;
   bool isLoading = true;
   bool isError = false;
+  bool isExpanded = false;
+  bool isFav = false;
+  List<Categorie> favorites = [];
+
+  void toggleFavorite(Categorie categorie) {
+    setState(() {
+      if (favorites.any((item) => item.id == categorie.id)) {
+        favorites.removeWhere((item) => item.id == categorie.id);
+      } else {
+        favorites.add(categorie);
+      }
+    });
+  }
 
   Future<void> getOneHost(id) async {
     try {
@@ -48,6 +65,8 @@ class _detailsState extends State<details> {
     return Scaffold(
       appBar: AppBar(title: Nav()),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
             height: 400,
@@ -60,17 +79,134 @@ class _detailsState extends State<details> {
                     horizontal: 5,
                     vertical: 15,
                   ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      oneHost.images[index],
-                      fit: BoxFit.cover,
-                    ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            oneHost.images[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 9,
+                        right: 8,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isFav = !isFav;
+                            });
+
+                            Categorie favoriteItem = Categorie(
+                              id: oneHost.id,
+                              name: oneHost.name.en,
+                              imgUrl: oneHost.images[0],
+                              description:
+                                  oneHost
+                                      .description
+                                      .en, // استخدام الوصف الممرر
+                            );
+                            toggleFavorite(favoriteItem);
+                          },
+                          icon:
+                              !isFav
+                                  ? Icon(
+                                    Icons.favorite_border,
+                                    color: Colors.grey[400],
+                                  )
+                                  : Icon(Icons.favorite),
+                          color: Color(0xFF003B95),
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
             ),
           ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            child: Wrap(
+              spacing: 20,
+              runSpacing: 10,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.location_on_rounded, color: Color(0xFF003B95)),
+                    SizedBox(width: 5),
+                    Text(oneHost.location.address.en),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.airline_seat_individual_suite_rounded,
+                      color: Color(0xFF003B95),
+                    ),
+                    SizedBox(width: 5),
+                    Text(oneHost.location.country.en),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.location_city, color: Color(0xFF003B95)),
+                    SizedBox(width: 5),
+                    Text(oneHost.location.city.en),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+
+                  children: [
+                    Icon(Icons.phone_android_rounded, color: Color(0xFF003B95)),
+
+                    Text("${oneHost.phone}"),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+
+                  children: [
+                    RatingBarIndicator(
+                      rating: 3.5,
+                      itemBuilder:
+                          (context, index) =>
+                              Icon(Icons.star, color: Colors.amber),
+                      itemCount: 5,
+                      itemSize: 30.0,
+                      direction: Axis.horizontal,
+                    ),
+                    Text("${oneHost.averageRating}"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: MaterialButton(
+                color: Color(0xFF003B95),
+                textColor: Colors.white,
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.chat_rounded, color: Colors.white),
+                    SizedBox(width: 10),
+                    Text("Chat With Owner"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
           Expanded(
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
@@ -80,7 +216,7 @@ class _detailsState extends State<details> {
                   Container(
                     margin: EdgeInsetsDirectional.only(bottom: 10),
                     child: Text(
-                      "${oneHost.name.en}",
+                      oneHost.name.en,
                       style: GoogleFonts.lora(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
@@ -94,7 +230,7 @@ class _detailsState extends State<details> {
                     margin: EdgeInsetsDirectional.only(bottom: 10),
 
                     child: Text(
-                      "${oneHost.subDescription.en}",
+                      oneHost.subDescription.en,
                       style: GoogleFonts.lora(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
@@ -104,25 +240,67 @@ class _detailsState extends State<details> {
                       overflow: TextOverflow.clip,
                     ),
                   ),
+
                   Container(
                     margin: EdgeInsetsDirectional.only(bottom: 10),
-
-                    child: Text(
-                      "${oneHost.description.en}",
-                      style: GoogleFonts.lora(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      ),
-                      maxLines: 100,
-                      overflow: TextOverflow.clip,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          oneHost.description.en,
+                          style: GoogleFonts.lora(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                          maxLines: isExpanded ? null : 3,
+                          overflow: TextOverflow.fade,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: TextButton(
+                            onPressed: () {
+                              setState(() {
+                                isExpanded = !isExpanded;
+                              });
+                            },
+                            child: Text(
+                              isExpanded ? "عرض أقل" : "عرض المزيد",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
           ),
+          Footer(),
         ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (favorites.isNotEmpty) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder:
+                    (context) => Favor(id: "Favorites", favorites: favorites),
+              ),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('لا توجد عناصر مفضلة لعرضها!')),
+            );
+          }
+        },
+        child: Icon(Icons.favorite),
       ),
     );
   }
